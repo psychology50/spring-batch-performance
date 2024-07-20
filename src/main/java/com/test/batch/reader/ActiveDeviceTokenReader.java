640cb6b7f6efc8969d9fce3ce0c6c1fc83682ab3
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-20 23:14:16 +0900

    feat: jdbc paging item reader bean add

--- src/main/java/com/test/batch/reader/ActiveDeviceTokenReader.java
+++ src/main/java/com/test/batch/reader/ActiveDeviceTokenReader.java
@@ -6,10 +6,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.data.RepositoryItemReader;
 import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
+import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.Sort;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
 import java.util.HashMap;
 
 @Slf4j
@@ -29,5 +34,29 @@ public class ActiveDeviceTokenReader {
                     put("id", Sort.Direction.ASC);
                 }})
                 .build();
+    }
+
+    @Bean
+    public JdbcPagingItemReader<DeviceTokenOwner> jdbcPagingItemReader(DataSource dataSource) {
+        SqlPagingQueryProviderFactoryBean factoryBean = new SqlPagingQueryProviderFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setSelectClause("SELECT u.id, u.name, dt.token");
+        factoryBean.setFromClause("FROM device_token dt LEFT JOIN user u ON dt.user_id = u.id");
+        factoryBean.setWhereClause("WHERE dt.activated = true AND u.account_book_notify = true");
+        factoryBean.setSortKey("u.id");
+
+        try {
+            return new JdbcPagingItemReaderBuilder<DeviceTokenOwner>()
+                    .name("jdbcPagingItemReader")
+                    .dataSource(dataSource)
+                    .fetchSize(100)
+                    .rowMapper(new BeanPropertyRowMapper<>(DeviceTokenOwner.class))
+                    .queryProvider(factoryBean.getObject())
+                    .pageSize(100)
+                    .build();
+        } catch (Exception e) {
+            log.error("Error creating jdbcPagingItemReader", e);
+            return null;
+        }
     }
 }
