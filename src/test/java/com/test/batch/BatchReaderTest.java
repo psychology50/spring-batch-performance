d3bd36ef86b537d1396052d4c798fb860a21991f
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-07-22 17:49:07 +0900

    test: jdbc cursor item reader

--- src/test/java/com/test/batch/BatchReaderTest.java
+++ src/test/java/com/test/batch/BatchReaderTest.java
@@ -2,14 +2,14 @@ package com.test.batch;
 
 import com.test.batch.dto.DeviceTokenOwner;
 import com.test.batch.reader.ActiveDeviceTokenReader;
-import com.test.batch.repository.DeviceTokenRepository;
 import com.test.batch.repository.UserRepository;
 import com.test.batch.supporter.DataCreator;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.data.RepositoryItemReader;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,6 +17,7 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StopWatch;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.fail;
@@ -25,47 +26,77 @@ import static org.assertj.core.api.Assertions.fail;
 @SpringBootTest
 @SpringBatchTest
 class BatchReaderTest {
-	@Autowired
-	private ActiveDeviceTokenReader reader;
+    @Autowired
+    private ActiveDeviceTokenReader reader;
 
-	@Autowired
-	private NamedParameterJdbcTemplate jdbcTemplate;
-	@Autowired
-	private UserRepository userRepository;
-	@Autowired
-	private DeviceTokenRepository deviceTokenRepository;
+    @Autowired
+    private NamedParameterJdbcTemplate jdbcTemplate;
+    @Autowired
+    private UserRepository userRepository;
 
-	private DataCreator dataCreator;
+    @Autowired
+    private DataSource dataSource;
 
-	@BeforeEach
-	void setUp() {
-		dataCreator = new DataCreator(jdbcTemplate);
-	}
+    private DataCreator dataCreator;
 
-	@Test
-	@Transactional
-	void testReaderPerformance() {
-		int userCount = 10, deviceTokenCount = userCount * 10;
-		dataCreator.bulkInsertUser(userCount);
-		List<Long> userIds = userRepository.findAllIds();
-		dataCreator.bulkInsertDeviceToken(deviceTokenCount, userIds);
+    @BeforeEach
+    void setUp() {
+        dataCreator = new DataCreator(jdbcTemplate);
+    }
 
-		StopWatch stopWatch = new StopWatch();
-		stopWatch.start();
+    @Test
+    @DisplayName("JdbcCursorItemReader 테스트")
+    void testJdbcCursorItemReader() throws Exception {
+        int userCount = 100, deviceTokenCount = userCount * 10;
+        dataCreator.bulkInsertUser(userCount);
+        List<Long> userIds = userRepository.findAllIds();
+        dataCreator.bulkInsertDeviceToken(deviceTokenCount, userIds);
 
-		RepositoryItemReader<DeviceTokenOwner> itemReader = reader.execute();
-		int count = 0;
-		DeviceTokenOwner item;
-		try {
-			while ((item = itemReader.read()) != null) {
-				log.info("Read item: {}", item);
-				count++;
-			}
-		} catch (Exception e) {
-			fail("Error reading items", e);
-		}
+        JdbcCursorItemReader<DeviceTokenOwner> itemReader = reader.jdbcCursorItemReader(dataSource);
+        itemReader.afterPropertiesSet();
 
-		stopWatch.stop();
-		log.info("Reader processed {} items in {} ms", count, stopWatch.getTotalTimeMillis());
-	}
+        itemReader.open(new ExecutionContext());
+
+        testItemReader(itemReader, "JdbcCursorItemReader");
+    }
+
+    @Test
+    @Disabled
+    @Transactional
+    @DisplayName("RepositoryItemReader 테스트")
+    void testRepositoryItemReader() {
+        int userCount = 100, deviceTokenCount = userCount * 10;
+        dataCreator.bulkInsertUser(userCount);
+        List<Long> userIds = userRepository.findAllIds();
+        dataCreator.bulkInsertDeviceToken(deviceTokenCount, userIds);
+
+        RepositoryItemReader<DeviceTokenOwner> itemReader = reader.execute();
+
+        testItemReader(itemReader, "RepositoryItemReader");
+    }
+
+    private void testItemReader(ItemReader<DeviceTokenOwner> itemReader, String readerName) {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        int count = 0;
+        DeviceTokenOwner item;
+        try {
+            while ((item = itemReader.read()) != null) {
+                log.info("{}", item);
+                count++;
+            }
+        } catch (Exception e) {
+            fail("Error reading items", e);
+        }
+
+        stopWatch.stop();
+        log.info("{} processed {} items in {} ms", readerName, count, stopWatch.getTotalTimeMillis());
+    }
+
+    @AfterEach
+    void tearDown() {
+        dataCreator.bulkDeleteDeviceToken();
+        dataCreator.bulkDeleteUser();
+    }
 }
